3bcab05659bc6a253813a1a9b35030b5f5732a82
ptsfdtz <pitousanfadetuzi@gmail.com>
2025-12-05 14:47:56 +0800

    feat: project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import styles from './App.module.css';
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.app}>
+      <div className={styles.sider}>
+        <div className={styles.logo}>logo</div>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.header}>header</div>
+        <div className={styles.content}>content</div>
+      </div>
+      <div className={styles.footer}>footer</div>
+    </div>
+  );
 }
 
 export default App;
