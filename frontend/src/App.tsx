25b202df4f83b933ccd3d09f153a6f4ff6a69802
ptsfdtz <pitousanfadetuzi@gmail.com>
2025-12-31 15:27:51 +0800

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>LiteSound</h1>
+            <p>Ready to build.</p>
+        </div>
+    )
+}
+
+export default App
