49fbb89d6ad48da8e026b34ca261ddd279ef8035
ptsfdtz <pitousanfadetuzi@gmail.com>
2026-01-06 14:28:08 +0800

    style: 更新对话面板和输入框背景颜色，统一样式

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 160},
 		OnStartup:        app.startup,
 		OnShutdown:       app.shutdown,
 		Bind: []interface{}{
